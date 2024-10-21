4482a48120709f32ef1118cdb1feae38097bfb82
shashank01sj <shashank@sportjacks.com>
2024-10-21 22:46:11 +0530

    added service according to the controller reqs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -35,4 +35,51 @@ export class UsersService {
             "role": "ADMIN",
         }
     ]
+
+    findAll(role?: 'INTERN' | 'USER' | 'ADMIN') {
+        if(role){
+            return this.users.filter(user=>user.role===role)
+        }
+        return this.users;
+
+    }
+    
+    findOne(id:number){
+        const user = this.users.find(user=>user.id ==id);
+        return user;
+    }
+
+
+    create(user:{name:string, email:string, role:'INTERN' | 'USER' | 'ADMIN'}){
+       const userByHighestId=[...this.users].sort((a,b)=> b.id - a.id);
+
+       const newUser ={
+        id:userByHighestId[0].id+1,
+        ...user
+       }
+       this.users.push(newUser);
+       return newUser;
+    }
+
+    update(id:number, updatedUser:{name?:string, email?:string, role?:'INTERN' | 'USER' | 'ADMIN'}){
+       this.users = this.users.map(user => {
+        if(user.id === id){
+            return {
+                ...user,
+                ...updatedUser
+            }
+        }
+        return user
+       })
+
+       return this.findOne(id);
+    }
+
+    delete(id:number){
+        const removedUser = this.findOne(id);
+
+        this.users = this.users.filter(user => user.id !== id);
+
+        return removedUser;
+    }
 }
