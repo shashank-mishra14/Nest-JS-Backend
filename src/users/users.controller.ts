f63ef070d407f81d71d496ccfdcf97be44bc95a6
shashank01sj <shashank@sportjacks.com>
2024-10-21 23:25:37 +0530

    adding DTO validation to the requests

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param,Patch,Post, Query } from '@nestjs/common';
 import { ParseIntPipe } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 @Controller('users')
 export class UsersController {
     constructor(private readonly usersService: UsersService) {}
@@ -16,13 +18,13 @@ export class UsersController {
     }
 
     @Post()
-    create(@Body() user:{name:string, email:string, role:'INTERN' | 'USER' | 'ADMIN'}){
-        return this.usersService.create(user);
+    create(@Body() createUserDto: CreateUserDto) {
+        return this.usersService.create(createUserDto);
     }
 
     @Patch(':id') //Patch /users/:id
-    update(@Param('id',ParseIntPipe) id: number, @Body() userUpdate:{name?:string, email?:string, role?:'INTERN' | 'USER' | 'ADMIN'}) {
-            return this.usersService.update(id, userUpdate);
+update(@Param('id',ParseIntPipe) id: number, @Body() updateUserDto: UpdateUserDto) {
+            return this.usersService.update(id, updateUserDto);
 
     }
 
