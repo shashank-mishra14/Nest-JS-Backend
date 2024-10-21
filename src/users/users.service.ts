8b0e4507db107ce5932147c185cb40cdb15145b7
shashank01sj <shashank@sportjacks.com>
2024-10-21 22:33:08 +0530

    adding prop

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users =[
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ]
+}
