844af2335b3c6762a50de72206fec2a40a3da5af
shashank01sj <shashank@sportjacks.com>
2024-10-21 22:28:23 +0530

    creating requests in controllers for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param,Patch,Post, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    @Get() //Get /users
+    findAll(@Query('role') role?:'INTERN' | 'USER' | 'ADMIN') {
+        return []
+    }
+
+    @Get() //Get /users/:id
+    findOne(@Param('id') id: string) {
+            return {id}
+
+    }
+
+    @Post()
+    create(@Body() user:{}){
+        return user;
+    }
+
+    @Patch(':id') //Patch /users/:id
+    update(@Param('id') id: string, @Body() userUpdate:{}) {
+            return {id,...userUpdate}
+
+    }
+
+    @Delete(':id') //Patch /users/:id
+    delete(@Param('id') id: string) {
+            return {id}
+
+    }
+}
