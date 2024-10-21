6874b87284f938937f84fcafec0f0e177069e83b
shashank01sj <shashank@sportjacks.com>
2024-10-21 22:53:43 +0530

    injected the services in the controlllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,33 +1,35 @@
 import { Body, Controller, Delete, Get, Param,Patch,Post, Query } from '@nestjs/common';
-
+import { UsersService } from './users.service';
 @Controller('users')
 export class UsersController {
-
+    constructor(private readonly usersService: UsersService) {}
     @Get() //Get /users
     findAll(@Query('role') role?:'INTERN' | 'USER' | 'ADMIN') {
-        return []
+        return this.usersService.findAll(role);
     }
 
     @Get() //Get /users/:id
     findOne(@Param('id') id: string) {
-            return {id}
+            return this.usersService.findOne(+id);
 
     }
 
     @Post()
-    create(@Body() user:{}){
-        return user;
+    create(@Body() user:{name:string, email:string, role:'INTERN' | 'USER' | 'ADMIN'}){
+        return this.usersService.create(user);
     }
 
     @Patch(':id') //Patch /users/:id
-    update(@Param('id') id: string, @Body() userUpdate:{}) {
-            return {id,...userUpdate}
+    update(@Param('id') id: string, @Body() userUpdate:{name?:string, email?:string, role?:'INTERN' | 'USER' | 'ADMIN'}) {
+            return this.usersService.update(+id, userUpdate);
 
     }
 
     @Delete(':id') //Patch /users/:id
     delete(@Param('id') id: string) {
-            return {id}
+            return this.usersService.delete(
+                +id
+            );
 
     }
 }
